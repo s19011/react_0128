69f1bf68861fb91b363a73492ff719232a75fefd
tenma <s19011@std.it-college.ac.jp>
2020-01-28 17:05:55 +0900

    0128

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import jsonp from 'fetch-jsonp'
+
+const getJSON = async uri => {
+  const result = await fetch(uri).then(res => res.json)
+  return result
+}
 
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { value: '' }
+    this.state = { value: {} }
+  }
+  componentDidMount () {
+    getJSON(
+      'https://heisenbug-premier-league-live-scores-v1.p.rapidapi.com/api/premierleague/players?team=Arsenal'
+    ).then(json => this.setState({ items: json }))
   }
   render () {
-    return (
-      <div>
-        <form onSubmit={e => this.doSubmit(e)}>
-          <input
-            type='text'
-            onChange={e => this.doChange(e)}
-            value={this.state.value}
-          />
-          <input type='submit' />
-        </form>
-      </div>
-    )
-  }
-  doChange (e) {
-    this.setState({ value: e.target.value })
-  }
-  dosubmit (e) {
-    e.preventDefault()
-    window.alert(this.state.value)
+    console.log(this.state)
+    return <div>test</div>
   }
 }
 export default App
