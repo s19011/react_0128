b5ff6e295cf194aac6d16621feedc4e372fd728d
tenma <s19011@std.it-college.ac.jp>
2020-01-28 15:06:14 +0900

    0128

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { value: '' }
+  }
+  render () {
+    return (
+      <div>
+        <form onSubmit={e => this.doSubmit(e)}>
+          <input
+            type='text'
+            onChange={e => this.doChange(e)}
+            value={this.state.value}
+          />
+          <input type='submit' />
+        </form>
+      </div>
+    )
+  }
+  doChange (e) {
+    this.setState({ value: e.target.value })
+  }
+  dosubmit (e) {
+    e.preventDefault()
+    window.alert(this.state.value)
+  }
 }
-
-export default App;
+export default App
