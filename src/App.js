30b9df7910ac8948e3f21a7bcebe4fb2e671b822
tenma <s19011@std.it-college.ac.jp>
2020-01-28 15:33:57 +0900

    template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { check: true }
+  }
+  render () {
+    //フォームにチェックボックスを指定
+    return (
+      <div>
+        <form onSubmit={e => this.doSubmit(e)}>
+          <label>
+            <input
+              type='checkbox'
+              onChange={e => this.doChange(e)}
+              checked={this.state.check}
+            />
+            食べる
+          </label>
+          <br />
+          <input type='submit' value='決定' />
+        </form>
+      </div>
+    )
+  }
+  //チェックボックスをクリックしたとき
+  doChange (e) {
+    this.setState({ check: !this.state.check })
+  }
+  //フォームを送信
+  doSubmit (e) {
+    e.preventDefault()
+    window.alert(this.state.check ? '食べる' : '食べない')
+  }
 }
 
-export default App;
+export default App
